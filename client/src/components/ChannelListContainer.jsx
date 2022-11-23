f974ad518fde967baf18f8e86bec5c162202279c
James Murphy <jamesm8228@gmail.com>
2022-11-23 18:35:59 -0500

    Add files for logos and icons - also import into container file

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,4 +1,21 @@
 import React from 'react'
+import {ChannelList, useChatContext} from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+
+import {ChannelSearch, TeamChannelList, TeamChannelPreview} from './';
+
+import HospitalIcon from '../assets/hospital.png'
+
+const SideBar = () =>{
+  <div className='channel-list__sidebar'>
+    <div className='channel-list__sidebar__icon1'>
+      <div className='icon1_inner'>
+        <img src={HospitalIcon} alt='Hospital' width='30'/>
+      </div>
+    </div>
+  </div>
+}
 
 const ChannelListContainer = () => {
   return (
