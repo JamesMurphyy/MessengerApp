1b6f78759b0f07c483e8aa4db4d54d797da801ac
James Murphy <jamesm8228@gmail.com>
2022-11-23 18:13:02 -0500

    Add imports for App jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
 
 const App = () => {
   return (
     <div>
       <h1> Real-Time Messenger Application</h1>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
