20eeb0c09fb0ce5bf0e6da63e3127793ab19e7f1
James Murphy <jamesm8228@gmail.com>
2022-11-25 18:14:01 -0500

    Add structure and inputs for account information

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -7,16 +7,19 @@ import signinImage from "../assets/signup.jpg";
 const Auth = () => {
   const [isSignup, setisSignup] = useState(false);
 
-  const handleChange = () => {}
+  const handleChange = () => {};
 
+  const switchMode = () => {
+    setisSignup((prevIsSignup) => !prevIsSignup);
+  };
   return (
     <div className="auth__form-container">
       <div className="auth__form-container_fields">
         <div className="auth__form-container_fields-content">
-          <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
-          <form onSubmit={()=>{}}>
+          <p>{isSignup ? "Sign Up" : "Sign In"}</p>
+          <form onSubmit={() => {}}>
             {isSignup && (
-              <div className="aut__form-container_fields-content_input">
+              <div className="auth__form-container_fields-content_input">
                 <label htmlFor="fullName">Full Name</label>
                 <input
                   name="fullName"
@@ -27,18 +30,75 @@ const Auth = () => {
                 />
               </div>
             )}
-            <div className="aut__form-container_fields-content_input">
-                <label htmlFor="username">Username</label>
+            <div className="auth__form-container_fields-content_input">
+              <label htmlFor="username">Username</label>
+              <input
+                name="username"
+                type="text"
+                placeholder="Username"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="phoneNumber">Phone Number</label>
                 <input
-                  name="username"
+                  name="phoneNumber"
                   type="text"
-                  placeholder="Username"
+                  placeholder="Phone Number"
                   onChange={handleChange}
                   required
                 />
               </div>
+            )}
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="avatarURL">Avatar URL</label>
+                <input
+                  name="avatarURL"
+                  type="text"
+                  placeholder="Avatar URL"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+            <div className="auth__form-container_fields-content_input">
+              <label htmlFor="password">Password</label>
+              <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                  name="confirmPassword"
+                  type="password"
+                  placeholder="Confirm Password"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
           </form>
+          <div className="auth__form-container_fields-account">
+            <p>
+              {isSignup ? "Already have an account?" : "Don't have account?"}
+              <span onClick={switchMode}>
+                {isSignup ? "Sign In" : "Sign Up"}
+              </span>
+            </p>
+          </div>
         </div>
+      </div>
+      <div className="auth__form-container_image">
+        <img src={signinImage} alt="sign in" />
       </div>
     </div>
   );
