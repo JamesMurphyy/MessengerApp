3bb19faf4ca0683cbaa7be52ea29359129cc5ab0
James Murphy <jamesm8228@gmail.com>
2022-11-28 11:42:29 -0500

    Add setup for server side index file

--- server/index.js
+++ server/index.js
@@ -1,2 +1,13 @@
-const express = require('express');
-const cors = requires('cors')
+const express = require("express");
+const cors = requires("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+
