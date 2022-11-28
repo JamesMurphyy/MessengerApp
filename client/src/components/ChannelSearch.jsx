37a5cec77e59f99f4e3adaa4e63a5d6e2a9601e3
James Murphy <jamesm8228@gmail.com>
2022-11-28 11:11:38 -0500

    Add cleanup for code

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -4,26 +4,24 @@ import { getChannel, useChatContext } from "stream-chat-react";
 import { SearchIcon } from "../assets";
 
 const ChannelSearch = () => {
-  const [query,setQuery] = useState('');
-  const [loading,setLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const getChannels = async (text) => {
     try {
       //TODO: Fetch Channels
+    } catch (error) {
+      setQuery("");
     }
-    catch (error) {
-      setQuery('');
-    }
-  }
+  };
 
   const onSearch = (e) => {
     e.preventDefault();
-    
-    setLoading(true);
-    setQuery(e.target.value)
-    getChannels(e.target.value)
-  }
 
+    setLoading(true);
+    setQuery(e.target.value);
+    getChannels(e.target.value);
+  };
 
   return (
     <div className="channel-search__container">
@@ -31,11 +29,13 @@ const ChannelSearch = () => {
         <div className="channel-search__input__icon">
           <SearchIcon />
         </div>
-        <input 
-        className="channel-search__input__text" 
-        placeholder="Search" 
-        type='text' 
-        value={query} onChange={onSearch} />
+        <input
+          className="channel-search__input__text"
+          placeholder="Search"
+          type="text"
+          value={query}
+          onChange={onSearch}
+        />
       </div>
     </div>
   );
