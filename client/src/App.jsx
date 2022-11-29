781291ca13293290e0f1eedbc8942dc72dbce8c3
James Murphy <jamesm8228@gmail.com>
2022-11-29 18:15:46 -0500

    Add logic to connect user and allows users to share messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,14 +26,13 @@ const client = StreamChat.getInstance(apiKey);
 
 if (authToken) {
   client.connectUser({
-    token: cookies.get("token"),
-    username: cookies.get("username"),
+    id: cookies.get("userId"),
+    name: cookies.get("username"),
     fullName: cookies.get("fullName"),
-    userId: cookies.get("userId"),
-    phoneNumber: cookies.get("phoneNumber"),
-    avatarURL: cookies.get("avatarURL"),
+    image: cookies.get("avatarURL"),
     hashedPassword: cookies.get("hashedPassword"),
-  });
+    phoneNumber: cookies.get("phoneNumber"),
+  }, authToken);
 }
 // const authToken = false;
 
