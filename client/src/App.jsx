88249d2b841c17172eef80f4bcab6cd2eaffb693
James Murphy <jamesm8228@gmail.com>
2022-11-30 19:10:46 -0500

    Add props for editing and creating through channel list containers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
@@ -25,26 +25,43 @@ const authToken = cookies.get("token");
 const client = StreamChat.getInstance(apiKey);
 
 if (authToken) {
-  client.connectUser({
-    id: cookies.get("userId"),
-    name: cookies.get("username"),
-    fullName: cookies.get("fullName"),
-    image: cookies.get("avatarURL"),
-    hashedPassword: cookies.get("hashedPassword"),
-    phoneNumber: cookies.get("phoneNumber"),
-  }, authToken);
+  client.connectUser(
+    {
+      id: cookies.get("userId"),
+      name: cookies.get("username"),
+      fullName: cookies.get("fullName"),
+      image: cookies.get("avatarURL"),
+      hashedPassword: cookies.get("hashedPassword"),
+      phoneNumber: cookies.get("phoneNumber"),
+    },
+    authToken
+  );
 }
 // const authToken = false;
 
 const App = () => {
-  
+  const [createType, setCreateType] = useState("");
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
   if (!authToken) return <Auth />;
 
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
-        <ChannelListContainer />
-        <ChannelContainer />
+        <ChannelListContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+        <ChannelContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
+        />
       </Chat>
     </div>
   );
--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-import { Channel, useChatContext } from "stream-chat-react";
+import { Channel, useChat, useChatContext } from "stream-chat-react";
 
 import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from "./";
 
 const ChannelContainer = () => {
+  const { channel } = useChatContext();
+
+  if (isCreating) {
+    return (
+      
+    )
+  }
+
+  if (isEditing) {
+    return(
+
+    )
+  }
+
+
   return <div>ChannelContainer</div>;
 };
 
