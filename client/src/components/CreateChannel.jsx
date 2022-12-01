17824d41df81e82d26c46fc342ca174f0bde828a
James Murphy <jamesm8228@gmail.com>
2022-12-01 14:45:56 -0500

    Add a section to add channel for team

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Channel, useChatContext } from "stream-chat-react";
 
-const CreateChannel = () => {
+import { UserList } from "./";
+import { CloseCreateChannel } from "../assets";
+
+const ChannelNameInput = ({ channelName = "", setChannelName }) => {
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setChannelName(e.target.value);
+  };
   return (
-    <div>CreateChannel</div>
-  )
-}
+    <div className="channel-name-input__wrapper">
+      <p>Name</p>
+      <input
+        value={channelName}
+        onChange={handleChange}
+        placeholder="channel-name"
+      />
+      <p>Add Members</p>
+    </div>
+  );
+};
 
-export default CreateChannel
+const CreateChannel = ({ createType, setIsCreating }) => {
+  const [ChannelName, setChannelName] = useState("");
+
+  return (
+    <div className="create-channel__container">
+      <div className="create-channel__header">
+        <p>
+          {createType === "team"
+            ? "Create a New Channel"
+            : "Send a Direct Message"}
+        </p>
+        <CloseCreateChannel setIsCreating={setIsCreating} />
+      </div>
+      {createType === "team" && (
+        <ChannelNameInput
+          ChannelName={ChannelName}
+          setChannelName={setChannelName}
+        />
+      )}
+    </div>
+  );
+};
+
+export default CreateChannel;
