914944437bd07145243cfe0e7ab6f43e76a12209
James Murphy <jamesm8228@gmail.com>
2022-12-06 16:37:56 -0500

    Add structure to begin the edit channel component

--- client/src/components/EditChannel.jsx
+++ client/src/components/EditChannel.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { useChatContext } from "stream-chat-react";
 
 const EditChannel = () => {
-  return <div>EditChannel</div>;
+  return (
+    <div>
+      EditChannel
+    </div>
+  )
 };
 
 export default EditChannel;
